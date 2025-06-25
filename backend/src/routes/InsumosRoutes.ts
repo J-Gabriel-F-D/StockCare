97656df3f6413620f14d6836c9a8dccf4f974f84
Jose Gabriel <jgabrielfd0@gmail.com>
2025-06-25 16:40:48 -0300

    feat: add controller for 'Insumos' management

---
+++ backend/src/routes/InsumosRoutes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { InsumosController } from "../controllers/InsumosController";
+
+const router = Router();
+
+// Lista todos os insumos
+router.get("/", (req, res) => {
+  InsumosController.getInsumos(req, res);
+});
+
+// Cria um novo insumo
+router.post("/", (req, res) => {
+  InsumosController.createInsumo(req, res);
+});
+
+// Busca um insumo por ID
+router.get("/:id", (req, res) => {
+  InsumosController.getInsumoById(req, res);
+});
+
+// Atualiza um insumo existente
+router.put("/:id", (req, res) => {
+  InsumosController.updateInsumo(req, res);
+});
+
+// Deleta um insumo
+router.delete("/:id", (req, res) => {
+  InsumosController.deleteInsumo(req, res);
+});
+
+export default router;
