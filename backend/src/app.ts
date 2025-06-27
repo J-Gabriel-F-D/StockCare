432b93ba479a5263f2e50c695a376153abeeae7d
Jose Gabriel <jgabrielfd0@gmail.com>
2025-06-27 20:53:59 -0300

    feat: enhance app setup with new routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import insumosRoutes from "./routes/InsumosRoutes";
+import fornecedoresRoutes from "./routes/FornecedoresRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/insumos", insumosRoutes);
+app.use("/fornecedores", fornecedoresRoutes);
 
 export default app;
