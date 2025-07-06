1f6a1035bec2c0f02c893e36cf116082e6b0814c
Jose Gabriel <jgabrielfd0@gmail.com>
2025-07-06 15:20:57 -0300

    chore(app): register movimentacao routes in API entry point

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import insumosRoutes from "./routes/InsumosRoutes";
 import fornecedoresRoutes from "./routes/FornecedoresRoutes";
+import movimentacaoRouter from "./routes/MovimentacoesRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/insumos", insumosRoutes);
 app.use("/fornecedores", fornecedoresRoutes);
+app.use("/movimentacoes", movimentacaoRouter);
 
 export default app;
